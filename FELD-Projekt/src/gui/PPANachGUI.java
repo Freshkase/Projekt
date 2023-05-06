2794806b8aeb5e714f157e82f6878f3675774620
vklim <vklim@DESKTOP-V284R8G.localdomain>
2023-05-06 17:03:59 +0200

    Mes

--- FELD-Projekt/src/gui/PPANachGUI.java
+++ FELD-Projekt/src/gui/PPANachGUI.java
@@ -250,7 +250,7 @@ public class PPANachGUI extends JPanel {
             	else 
             	{
             	
-                   
+                    
             		JOptionPane.showMessageDialog(null, "BPS-Vortrag wurde schon auf ja gesetzt", "Informationen zum Unternehmen", JOptionPane.INFORMATION_MESSAGE);
 
             	}
