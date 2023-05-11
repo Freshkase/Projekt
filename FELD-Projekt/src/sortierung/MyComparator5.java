618ce08030387e25ae91060e41415602867bd2c1
frech <frech@10.90.36.78>
2023-05-11 11:41:35 +0200

    s

--- FELD-Projekt/src/sortierung/MyComparator5.java
+++ FELD-Projekt/src/sortierung/MyComparator5.java
@@ -8,6 +8,6 @@ public class MyComparator5 implements Comparator<Student> {
 	@Override
 	public int compare(Student o1, Student o2) {
 		// TODO Auto-generated method stub
-		return o2.getMatrikelnr()-o1.getMatrikelnr();
+		return o1.getMatrikelnr()-o2.getMatrikelnr();
 	}
 }
