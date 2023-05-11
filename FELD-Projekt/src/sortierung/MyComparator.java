ef9d9b0efbaf622e6af62d3b97177326968305ab
frech <frech@10.90.36.78>
2023-05-11 11:36:58 +0200

    s

--- FELD-Projekt/src/sortierung/MyComparator.java
+++ FELD-Projekt/src/sortierung/MyComparator.java
@@ -9,7 +9,7 @@ public class MyComparator implements Comparator<Student> {
 	@Override
 	public int compare(Student o1, Student o2) {
 		// TODO Auto-generated method stub
-		return o1.getBesuchsbericht().compareTo(o2.getBesuchsbericht());
+		return o2.getMatrikelnr()-o1.getMatrikelnr();
 	}
 
 }
---
+++ FELD-Projekt/src/sortierung/MyComparator5.java
@@ -0,0 +1,13 @@
+package sortierung;
+
+import java.util.Comparator;
+
+import objekte.Student;
+
+public class MyComparator5 implements Comparator<Student> {
+	@Override
+	public int compare(Student o1, Student o2) {
+		// TODO Auto-generated method stub
+		return o1.getBesuchsbericht().compareTo(o2.getBesuchsbericht());
+	}
+}
